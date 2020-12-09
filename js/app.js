3ef23631470e2b4837a248af36b922ae8600ba10
Miguel <miguelix90@hotmail.com>
2020-12-09 20:10:32 +0100

    Modificadas las rutas

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
+var url = window.location.href;
+var swLocation = '/twittor/sw.js';
+
 if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('/sw.js');
+
+    if (url.includes('.test')) {
+        swLocation = '/sw.js';
+    }
+
+    navigator.serviceWorker.register(swLocation);
 }
 
 
